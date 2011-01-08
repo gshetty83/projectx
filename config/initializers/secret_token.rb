33888f3051f8cc60522e31957039153bedfd5ae6
Gulshan Shetty <gulshan.shetty.83@gmail.com>
2011-01-08 02:58:52 -0500

    Exasperated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectx::Application.config.secret_token = 'fa5b50100a964c01bdf2de5ba457e16002e8886fdea6cf0d4be797fa4c12416509c164a642a25d0d12df41cc6ba6b4b8c8ca229d5b9847ea5525a595f87e3d3f'
